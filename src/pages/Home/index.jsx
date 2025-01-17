80d5a2184298841c8dace63e41eed73bc69e1daa
busayogabriel47 <omotukabusayo22@gmail.com>
2025-01-17 15:59:09 +0000

    new update

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -9,16 +9,21 @@ const ctaBg = "https://forcythe.com/images/header-background.svg";
 const LandingPage = () => {
   return (
     <div className="bg-[#030516] w-full">
+      {/* Hero Section */}
       <div
-        className="relative h-[600px] bg-cover bg-center flex items-center justify-center p-8"
+        className="relative h-[400px] sm:h-[500px] md:h-[600px] bg-cover bg-center flex items-center justify-center p-4 sm:p-8"
         style={{ backgroundImage: `url(${ctaBg})` }}
       >
         <CallToAction />
       </div>
-      <SuccessInMotion />
-      <Testimonials />
-      <SportLight/>
-      <ProductInnocation/>
+
+      {/* Other Sections */}
+      <div className="px-4 sm:px-8 md:px-12 lg:px-16 xl:px-24">
+        <SuccessInMotion />
+        <Testimonials />
+        <SportLight />
+        <ProductInnocation />
+      </div>
     </div>
   );
 };
